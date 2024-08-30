99914b5437328a1ac75e0abb1e8c08fc6fb0976d
Bhavesh37-art <bhaveshgadekar27@gmail.com>
2024-08-30 21:53:30 +0530

    mongoURL
    changed the url

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb://localhost:27017/';
+const mongoURI = 'mongodb+srv://bhaveshgadekar27:Bhavesh27@cluster0.kbnzj.mongodb.net/';
 
 const connectToMongo = async () => {
     try {
